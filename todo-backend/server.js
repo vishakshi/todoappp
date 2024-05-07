1a1dc7a3e99f06d905ed82bf940a335584b74247
Vishakshi Gupta <162357622+vishakshi@users.noreply.github.com>
2024-05-07 17:26:53 +0530

    Add files via upload

---
+++ todo-backend/server.js
@@ -0,0 +1,83 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://vishakshigupta790:7905166535@todo.pfq0coc.mongodb.net/?retryWrites=true&w=majority&appName=todo', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connection succesfullll '))
+  .catch(err => console.error(err));
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Todo Model
+const Todo = mongoose.model('Todo', {
+  text: String,
+});
+
+// Routes
+app.get('/api/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/api/todos', async (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  try {
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.delete('/api/todos/:id', async (req, res) => {
+    try {
+      const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+      if (!deletedTodo) {
+        return res.status(404).json({ message: 'Todo not found' });
+      }
+      res.json({ message: 'Todo deleted successfully' });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+
+  app.put('/api/todos/:id', async (req, res) => {
+    const { id } = req.params;
+    const updates = req.body;
+
+    try {
+        const updatedTodo = await Todo.findByIdAndUpdate(id, updates, { new: true });
+
+        if (!updatedTodo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+
+        res.json({ message: 'Todo updated successfully', updatedTodo });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
