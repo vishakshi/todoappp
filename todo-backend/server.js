3ea282b97b634c7ea8761dcaf0cdfc11384c3ea8
Vishakshi Gupta <162357622+vishakshi@users.noreply.github.com>
2024-05-07 18:07:04 +0530

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -8,10 +8,21 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// MongoDB Connection
-mongoose.connect('mongodb+srv://vishakshigupta790:7905166535@todo.pfq0coc.mongodb.net/?retryWrites=true&w=majority&appName=todo', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('connection succesfullll '))
-  .catch(err => console.error(err));
+require('dotenv').config(); // Load environment variables from .env file
+
+
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+
 
 // Middleware
 app.use(cors());
