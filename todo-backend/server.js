70e3f09aad99c2db743bf14851d02fc6a1872b25
Vishakshi Gupta <162357622+vishakshi@users.noreply.github.com>
2024-05-07 22:14:04 +0530

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 require('dotenv').config(); // Load environment variables from .env file
 
