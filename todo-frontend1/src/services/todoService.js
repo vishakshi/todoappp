3547fe4989aeea4c3fa47ee115c0d2454af37e50
Vishakshi Gupta <162357622+vishakshi@users.noreply.github.com>
2024-05-09 16:55:50 +0530

    Update todoService.js

--- todo-frontend1/src/services/todoService.js
+++ todo-frontend1/src/services/todoService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:5000/api/todos';
+const apiUrl = 'https://todoappp-4.onrender.com/api/todos';
 
 const getAllTodos = async () => {
   const response = await axios.get(apiUrl);
